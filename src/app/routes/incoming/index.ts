9a3bd349b42707acd4df9688c70231a9cc1aa102
multidevs <multiz.dev@gmail.com>
2024-12-11 13:30:02 +0500

    Refactor incoming call handling in router.
    Simplify and clean up `/answer-call` logic by re-enabling previously commented-out code blocks for call state creation and speech recognition. Removed unused `/issue` route and redundant logging for better clarity and maintainability.

--- src/app/routes/incoming/index.ts
+++ src/app/routes/incoming/index.ts
@@ -2,21 +2,13 @@ import { Router, Request, Response } from 'express';
 
 import Twilio from 'twilio';
 
-/*import { createCallState, resetCallState } from '#app/state/callManager.js';
+import { createCallState, resetCallState } from '#app/state/callManager.js';
 import { initialMessages } from '#app/utils/promptUtil.js';
 import speechClient from '#app/services/speechService.js';
 import { SPEECH_REQUEST } from '#app/config/config.mjs';
-import { handleSpeechData } from '#app/controllers/callController.js';*/
+import { handleSpeechData } from '#app/controllers/callController.js';
 
 const incomingRouter: Router = Router();
-
-incomingRouter.get('/issue', (req: any, res: any) => {
-  console.log("ISSUE", req.body);
-
-  return res.json({
-    test: JSON.stringify(req.body),
-  });
-});
 
 incomingRouter.post('/answer-call',(req: Request, res: Response) => {
   const callId = req.body.CallSid;
@@ -30,22 +22,12 @@ incomingRouter.post('/answer-call',(req: Request, res: Response) => {
 
     console.log("WS-URI-COMPLETE", callSpecificWsUri);
 
-    const connect = response.connect();
-
-    connect.stream({
-      url: callSpecificWsUri,
-      /*statusCallbackMethod: 'POST',
-      statusCallback: `${process.env.WS_SERVER_URI}/api/incoming/`*/
-    })
-
-    console.log('rsp', response.toString());
-
-    /*const callState = createCallState(callId);
+    const callState = createCallState(callId);
 
     callState.messages = [...initialMessages];
-    callState.currentCall = response;*/
+    callState.currentCall = response;
 
-    /*callState.recognizeStream = speechClient
+    callState.recognizeStream = speechClient
       .streamingRecognize(SPEECH_REQUEST)
       .on('error', (error) => {
         console.error('Error in Speech-to-Text:', error);
@@ -54,7 +36,13 @@ incomingRouter.post('/answer-call',(req: Request, res: Response) => {
       .on('end', () => {
         console.log('Recognize Stream Ended: ', callId);
         void resetCallState(callId)
-      });*/
+      });
+
+    const connect = response.connect();
+
+    connect.stream({
+      url: callSpecificWsUri,
+    })
 
     response.say("I say this after websocket connection ends");
 
